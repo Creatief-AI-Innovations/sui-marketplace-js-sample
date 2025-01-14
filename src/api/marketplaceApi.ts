6f87a69b1a9b1389c50096778c27cad3a94caddf
sinkingsheep <82883490+sinkingsheep@users.noreply.github.com>
2025-01-14 21:57:26 +0530

    Update marketplaceApi.ts

--- src/api/marketplaceApi.ts
+++ src/api/marketplaceApi.ts
@@ -4,6 +4,6 @@ console.log('REACT APP MARKETPLACE API URL');
 console.log(import.meta.env)
 
 export const marketplaceApi = new MarketplaceSDK({
-    baseURL: import.meta.env.VITE_MARKETPLACE_API_URL + 'api',
+    baseURL: import.meta.env.VITE_MARKETPLACE_API_URL + '/api',
     apiKey: import.meta.env.VITE_MARKETPLACE_API_KEY,
 });
